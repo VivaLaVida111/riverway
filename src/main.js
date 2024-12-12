20ea8092b9bfb9040152fc90235a3d78afc78e30
VivaLaVida111 <2310745915@qq.com>
2024-12-12 10:48:04 +0800

    version 0.0.1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import VueAMap, { initAMapApiLoader } from "@vuemap/vue-amap";
+import "@vuemap/vue-amap/dist/style.css";
+initAMapApiLoader({
+  key: "165ffdb04e5182122b110a5d3e9928e2",
+});
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 
 const app = createApp(App);
-app.use(ElementPlus);
-app.mount('#app');
+app.use(ElementPlus, { locale: zhCn });
+app.use(VueAMap);
+app.mount("#app");
